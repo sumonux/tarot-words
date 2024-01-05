07197afb10c8c621abceba829596d93548f0a877
olegdudkin <oleg@groundaround.com>
2024-01-05 13:31:49 -0600

    Update post header and title layout
    CSS classes and the order of elements in components 'post-header' and 'post-title' have been adjusted to improve layout. Changes in 'post-header' component include rearranging the post title, cover image, author, and date. The 'post-title' font size has been decreased for medium and large screens for better readability.

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -13,23 +13,30 @@ type Props = {
 
 const PostHeader = ({ title, coverImage, date, author }: Props) => {
   return (
-    <>
+    <div className="py-12">
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
+      <div className="hidden md:flex flex-wrap items-center">
+        <div className="md:block md:mb-12">
+          <Avatar name={author.name} picture={author.picture}/>
+        </div>
+        <div className="mb-12 text-lg px-6">
+          <DateFormatter dateString={date}/>
+        </div>
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
+        <CoverImage title={title} src={coverImage}/>
       </div>
       <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
+        <div className="flex md:hidden flex-wrap items-center">
+          <div className="md:block md:mb-12">
+            <Avatar name={author.name} picture={author.picture}/>
+          </div>
+          <div className="text-lg px-6">
+            <DateFormatter dateString={date}/>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
