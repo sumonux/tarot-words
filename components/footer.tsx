c3a4e8fc6977694bb8df0e1d545abf6641fa315a
olegdudkin <oleg@groundaround.com>
2024-01-05 13:31:57 -0600

    Improve text and image styles in components and styles
    The commit involves changing CSS classes to update layout and add color styles. The header and footer titles now have 'text-black' and 'hover:text-black'. Additionally, changes include decreasing the font size of the component 'footer' from '2.5rem' to '2rem'. The 'cover-image' component was modified to have a rounded

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <a href="/">Tarot Words</a> 2023.
+          <h3 className="text-2xl lg:text-[2rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            <a href="/" className="text-black hover:text-black">Tarot Words</a> 2023.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
 
--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <nav className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
+      <Link href="/" className="text-black hover:text-black">
         Tarot Words
       </Link>
       .
